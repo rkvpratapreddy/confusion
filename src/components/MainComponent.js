034cfd901cf17ef85025f402441c557c230a0cb7
Pratap <Pratap.itfriendz@gmail.com>
2022-10-05 21:55:15 +0530

    Exercise (Instructions): Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,41 +1,43 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-// import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import { Routes, Route, Link, Outlet } from 'react-router-dom';
-
+import { Routes, Route, Navigate } from 'react-router-dom';
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     }
   }
 
 
   render() {
-    const HomePage = () => {
-      return(
-          <Home 
-          />
-      );
-    }
     return (
       <div>
-       
-              <Header />
-                <Routes>
-                  <Route path='/home' element={HomePage} />
-                  <Route exact path='/menu' element={() => <Menu dishes={this.state.dishes} />} />
-                  {/* <Route to="/" component={<Navigate replace to="/home" />}/> */}
-                </Routes>
-                
-              <Footer />
+        <Header />
+          <Routes>
+            <Route path="/" element={<Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+              />} />
+            <Route path="/menu" element={<Menu dishes={this.state.dishes} />} />
+            <Route exact path='/contactus' element={<Contact />} />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        <Footer />
       </div>
     );
   }
